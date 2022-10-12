8e00025774728183024ec707406d4fa9c020280e
ShreyaSridhar04 <90780275+ShreyaSridhar04@users.noreply.github.com>
2022-10-11 22:34:48 -0500

    got everythung to work except try catch

--- app/src/main/java/com/example/mymemorybox/SignInActivity.java
+++ app/src/main/java/com/example/mymemorybox/SignInActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuthEmailException;
 
 public class SignInActivity extends AppCompatActivity  {
 
@@ -84,9 +85,10 @@ public class SignInActivity extends AppCompatActivity  {
                                 startActivity(intent);
                             }
                             else {
-                                // if sign up fails, display a message to the user along with the exception from firebase auth
-                                Log.d(TAG, "Sign up failed for " + userName + " " + password +
-                                        " because of \n"+ task.getResult());
+
+   // if sign up fails, display a message to the user along with the exception from firebase auth
+Log.d(TAG, "Sign up failed for " + userName + " " + password +
+       " because of \n"+ task.getResult());
 
                             }
                         }
@@ -159,8 +161,24 @@ public class SignInActivity extends AppCompatActivity  {
             return false;
         } else {
             Log.i(TAG, userName + " " + password + " is set after getValues(), return true");
+            userName = removeTrailingSpaces(userName);
             return true;
         }
+    }
+    /**
+     * This method accepts the email the user wants to submit for FirebaseAuth
+     * and removes an extra spaces that may have accidentally been added at the end by
+     * the auto-correct keyboard.  This typically happens when the email is used all
+     * the time and shows up as a suggestion for the user.
+     *
+     * @param email
+     * @return a String without trailing spaces
+     */
+    private String removeTrailingSpaces(String email) {
+        String lastChar = email.substring(email.length() -1);
+        if (lastChar.equals(" "))
+            email = email.substring(0, email.length()-1);
+        return email;
     }
 
 }
