a2f00ad688ffc2785078476c7a8aefdc7ab2b166
ShreyaSridhar04 <90780275+ShreyaSridhar04@users.noreply.github.com>
2022-10-17 12:49:50 -0500

    connected to firestore database
    memories are showing up but ratings are showing up as 0

--- app/src/main/java/com/example/mymemorybox/AddMemoryActivity.java
+++ app/src/main/java/com/example/mymemorybox/AddMemoryActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -18,6 +19,8 @@ public class AddMemoryActivity extends AppCompatActivity implements AdapterView.
     // https://www.youtube.com/watch?v=7tnlh1nVkuE
 
     Spinner spinner;
+    EditText memoryName, memoryDesc;
+    String spinnerSelectedText = "none";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +36,9 @@ public class AddMemoryActivity extends AppCompatActivity implements AdapterView.
 
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+        memoryName = findViewById(R.id.memNameEditText);
+        memoryDesc = findViewById(R.id.memoryDescEditText);
     }
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -42,6 +48,31 @@ public class AddMemoryActivity extends AppCompatActivity implements AdapterView.
     // This method is required, even if empty, for the OnItemSelectedListener to work
     @Override
     public void onNothingSelected(AdapterView<?> parent) { }
+    public void addMemoryButtonClicked(View view) {
+        String memName = memoryName.getText().toString();
+        String memDesc = memoryDesc.getText().toString();
+        int memoryRatingNum = 0;
+        // This will take the option they clicked on and ensure it is a number.
+// My options went from 5 to 1, so that is why I have it adjusted with 6-i
+// I also had an instruction statement as my first line in my string array
+// ADJUST THIS LOOP TO MATCH YOUR CODE!
+
+// Note the syntax here for how to access an index of a string array within
+// the java
+        for (int i = 1; i < 6; i++) {
+            if (spinnerSelectedText.equals(getResources().
+                    getStringArray(R.array.memoryRating)[i])) {
+                memoryRatingNum = 6-i;
+                break;
+            }
+        }
+
+        Memory m = new Memory(memoryRatingNum, memName, memDesc);
+        SignInActivity.firebaseHelper.addData(m);
+
+        memoryName.setText("");
+        memoryDesc.setText("");
+    }
 
 
 }
